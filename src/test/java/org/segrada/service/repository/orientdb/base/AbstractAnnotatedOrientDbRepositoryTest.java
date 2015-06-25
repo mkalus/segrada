b478c45a55aff3169c16a3af7b348f150c38e123
Maximilian Kalus <info@auxnet.de>
2015-06-25 16:18:42 +0200

    Implement mock repositories for tests.

--- src/test/java/org/segrada/service/repository/orientdb/base/AbstractAnnotatedOrientDbRepositoryTest.java
+++ src/test/java/org/segrada/service/repository/orientdb/base/AbstractAnnotatedOrientDbRepositoryTest.java
@@ -1,10 +1,69 @@
 package org.segrada.service.repository.orientdb.base;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.segrada.model.base.AbstractAnnotatedModel;
+import org.segrada.model.prototype.SegradaAnnotatedEntity;
+import org.segrada.service.repository.orientdb.factory.OrientDbRepositoryFactory;
+import org.segrada.session.Identity;
+import org.segrada.test.OrientDBTestInstance;
+import org.segrada.test.OrientDbTestApplicationSettings;
 
 import static org.junit.Assert.*;
 
 public class AbstractAnnotatedOrientDbRepositoryTest {
+	/**
+	 * reference to test instance of orientdb in memory
+	 */
+	private OrientDBTestInstance orientDBTestInstance = new OrientDBTestInstance();
+
+	/**
+	 * mock repository - see below
+	 */
+	private MockOrientDbRepository mockOrientDbRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		// set up schema if needed
+		orientDBTestInstance.setUpSchemaIfNeeded();
+
+		// open database
+		ODatabaseDocumentTx db = orientDBTestInstance.getDatabase();
+
+		// create schema
+		db.command(new OCommandSQL("create class Mock extends V")).execute();
+
+		OrientDbRepositoryFactory factory = new OrientDbRepositoryFactory(db, new OrientDbTestApplicationSettings(), new Identity());
+
+		// create repo
+		mockOrientDbRepository = new MockOrientDbRepository(factory);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// close db
+		try {
+			mockOrientDbRepository.db.close();
+		} catch (Exception e) {
+			// do nothing
+		}
+
+		// open database
+		ODatabaseDocumentTx db = orientDBTestInstance.getDatabase();
+
+		// remove schema
+		db.command(new OCommandSQL("delete vertex V")).execute();
+		db.command(new OCommandSQL("delete edge E")).execute();
+		db.command(new OCommandSQL("truncate class Pictogram")).execute();
+		db.command(new OCommandSQL("drop class Mock")).execute();
+
+		// close db
+		db.close();
+	}
 
 	@Test
 	public void testPopulateODocumentWithAnnotated() throws Exception {
@@ -52,5 +111,53 @@ public class AbstractAnnotatedOrientDbRepositoryTest {
 	public void testDelete() throws Exception {
 		fail("TODO: check, if needed!");
 
+	}
+
+	/**
+	 * Mock entity
+	 */
+	private class MockEntity extends AbstractAnnotatedModel implements SegradaAnnotatedEntity {
+		private String id;
+
+		@Override
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public String getId() {
+			return id;
+		}
+
+		@Override
+		public String getTitle() {
+			return "DUMMY";
+		}
+	}
+
+	/**
+	 * Partial/mock repository to test methods
+	 */
+	private class MockOrientDbRepository extends AbstractAnnotatedOrientDbRepository<MockEntity> {
+		public MockOrientDbRepository(OrientDbRepositoryFactory repositoryFactory) {
+			super(repositoryFactory);
+		}
+
+		@Override
+		public MockEntity convertToEntity(ODocument document) {
+			MockEntity entity = new MockEntity();
+			populateEntityWithBaseData(document, entity);
+			return entity;
+		}
+
+		@Override
+		public ODocument convertToDocument(MockEntity entity) {
+			return createOrLoadDocument(entity);
+		}
+
+		@Override
+		public String getModelClassName() {
+			return "Mock";
+		}
 	}
 }
--- src/test/java/org/segrada/service/repository/orientdb/base/AbstractCoreOrientDbRepositoryTest.java
+++ src/test/java/org/segrada/service/repository/orientdb/base/AbstractCoreOrientDbRepositoryTest.java
@@ -1,13 +1,69 @@
 package org.segrada.service.repository.orientdb.base;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.orientechnologies.orient.core.sql.OCommandSQL;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.segrada.model.base.AbstractCoreModel;
+import org.segrada.model.prototype.SegradaCoreEntity;
+import org.segrada.service.repository.orientdb.factory.OrientDbRepositoryFactory;
+import org.segrada.session.Identity;
+import org.segrada.test.OrientDBTestInstance;
+import org.segrada.test.OrientDbTestApplicationSettings;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by mkalus on 25.06.15.
- */
 public class AbstractCoreOrientDbRepositoryTest {
+	/**
+	 * reference to test instance of orientdb in memory
+	 */
+	private OrientDBTestInstance orientDBTestInstance = new OrientDBTestInstance();
+
+	/**
+	 * mock repository - see below
+	 */
+	private MockOrientDbRepository mockOrientDbRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		// set up schema if needed
+		orientDBTestInstance.setUpSchemaIfNeeded();
+
+		// open database
+		ODatabaseDocumentTx db = orientDBTestInstance.getDatabase();
+
+		// create schema
+		db.command(new OCommandSQL("create class Mock extends V")).execute();
+
+		OrientDbRepositoryFactory factory = new OrientDbRepositoryFactory(db, new OrientDbTestApplicationSettings(), new Identity());
+
+		// create repo
+		mockOrientDbRepository = new MockOrientDbRepository(factory);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// close db
+		try {
+			mockOrientDbRepository.db.close();
+		} catch (Exception e) {
+			// do nothing
+		}
+
+		// open database
+		ODatabaseDocumentTx db = orientDBTestInstance.getDatabase();
+
+		// remove schema
+		db.command(new OCommandSQL("delete vertex V")).execute();
+		db.command(new OCommandSQL("delete edge E")).execute();
+		db.command(new OCommandSQL("truncate class Pictogram")).execute();
+		db.command(new OCommandSQL("drop class Mock")).execute();
+
+		// close db
+		db.close();
+	}
 
 	@Test
 	public void testPopulateODocumentWithCore() throws Exception {
@@ -37,5 +93,53 @@ public class AbstractCoreOrientDbRepositoryTest {
 	public void testDelete() throws Exception {
 		fail();
 
+	}
+
+	/**
+	 * Mock entity
+	 */
+	private class MockEntity extends AbstractCoreModel implements SegradaCoreEntity {
+		private String id;
+
+		@Override
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public String getId() {
+			return id;
+		}
+
+		@Override
+		public String getTitle() {
+			return "DUMMY";
+		}
+	}
+
+	/**
+	 * Partial/mock repository to test methods
+	 */
+	private class MockOrientDbRepository extends AbstractCoreOrientDbRepository<MockEntity> {
+		public MockOrientDbRepository(OrientDbRepositoryFactory repositoryFactory) {
+			super(repositoryFactory);
+		}
+
+		@Override
+		public MockEntity convertToEntity(ODocument document) {
+			MockEntity entity = new MockEntity();
+			populateEntityWithBaseData(document, entity);
+			return entity;
+		}
+
+		@Override
+		public ODocument convertToDocument(MockEntity entity) {
+			return createOrLoadDocument(entity);
+		}
+
+		@Override
+		public String getModelClassName() {
+			return "Mock";
+		}
 	}
 }
