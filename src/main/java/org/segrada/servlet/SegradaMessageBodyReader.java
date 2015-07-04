f523156de256b2949b132dea82e90d565ccfaf28
Maximilian Kalus <info@auxnet.de>
2015-07-04 16:59:45 +0200

    Fix message body reader to load entity from db when updating.

--- src/main/java/org/segrada/servlet/SegradaMessageBodyReader.java
+++ src/main/java/org/segrada/servlet/SegradaMessageBodyReader.java
@@ -10,6 +10,7 @@ import org.segrada.service.base.AbstractRepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -24,6 +25,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Provider
@@ -44,13 +46,47 @@ public class SegradaMessageBodyReader implements MessageBodyReader<SegradaEntity
 
 	@Override
 	public SegradaEntity readFrom(Class<SegradaEntity> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> multivaluedMap, InputStream inputStream) throws IOException, WebApplicationException {
+		// read form data
+		StringWriter writer = new StringWriter();
+		IOUtils.copy(inputStream, writer, "UTF-8");
+		String theString = writer.toString();
+
+		// parse form data to key/value pairs
+		List<NameValuePair> parameters = URLEncodedUtils.parse(theString, Charset.forName("UTF-8"));
+		String id = null;
+		// find id
+		for (NameValuePair nameValuePair : parameters) {
+			if (nameValuePair.getName().equals("id")) {
+				id = nameValuePair.getValue();
+				break;
+			}
+		}
+
 		// create instance
 		SegradaEntity entity;
-		try {
-			entity = aClass.newInstance();
-		} catch (Exception e) {
-			logger.error("Could not convert to entity: " + aClass, e);
-			return null;
+		// load instance from repository?
+		if (id != null) {
+			AbstractRepositoryService service = getServiceForClassName(aClass.getSimpleName());
+			if (service == null) {
+				logger.error("No service for: " + aClass);
+				return null;
+			}
+			entity = service.findById(id);
+			if (entity == null) {
+				logger.error("Could not load id " + id + " as entity: " + aClass);
+				return null;
+			}
+			if (logger.isDebugEnabled())
+				logger.debug("Loaded existing entity for update: " + entity.toString());
+		} else {
+			try {
+				entity = aClass.newInstance();
+			} catch (Exception e) {
+				logger.error("Could not convert to entity: " + aClass, e);
+				return null;
+			}
+			if (logger.isDebugEnabled())
+				logger.debug("Loaded new entity for creation: " + entity.toString());
 		}
 
 		// map setter methods to value map
@@ -63,13 +99,11 @@ public class SegradaMessageBodyReader implements MessageBodyReader<SegradaEntity
 			}
 		}
 
-		// read form data
-		StringWriter writer = new StringWriter();
-		IOUtils.copy(inputStream, writer, "UTF-8");
-		String theString = writer.toString();
-
 		// get each form key and value
-		for (NameValuePair nameValuePair : URLEncodedUtils.parse(theString, Charset.forName("UTF-8"))) {
+		for (NameValuePair nameValuePair : parameters) {
+			// do not update id
+			if (nameValuePair.getName().equals("id")) continue;
+
 			Method setter = setters.get(nameValuePair.getName());
 			if (setter != null) {
 				// get first parameter of setter (type to cast)
@@ -92,11 +126,8 @@ public class SegradaMessageBodyReader implements MessageBodyReader<SegradaEntity
 				if (SegradaEntity.class.isAssignableFrom(setterType)) {
 					if (value != null && !((String)value).isEmpty()) {
 						try { // dynamically determine service and fetch element by id
-							String className = setterType.getSimpleName();
-							if (className.startsWith("I")) className = className.substring(1);
-
-							Class serviceClass = Class.forName("org.segrada.service." + className + "Service");
-							AbstractRepositoryService service = (AbstractRepositoryService) injector.getInstance(serviceClass);
+							AbstractRepositoryService service = getServiceForClassName(setterType.getSimpleName());
+							if (service == null) throw new Exception("Empty service.");
 							value = service.findById((String) value);
 						} catch (Exception e) {
 							logger.error("Error getting/setting SegradaEntity reference of type " + setterType + ", value: " + value, e);
@@ -137,6 +168,26 @@ public class SegradaMessageBodyReader implements MessageBodyReader<SegradaEntity
 			}
 		}
 
+		if (logger.isDebugEnabled())
+			logger.debug("Updated data of entity: " + entity.toString());
+
 		return entity;
+	}
+
+	/**
+	 * get service for (simple) class name
+	 * @param className simple class name of model, e.g. Node or INode
+	 * @return service or null
+	 */
+	private @Nullable AbstractRepositoryService getServiceForClassName(String className) {
+		try {
+			if (className.startsWith("I")) className = className.substring(1);
+
+			Class serviceClass = Class.forName("org.segrada.service." + className + "Service");
+			return (AbstractRepositoryService) injector.getInstance(serviceClass);
+		} catch (Exception e) {
+			logger.error("Error retrieving service for class " + className, e);
+		}
+		return null;
 	}
 }
