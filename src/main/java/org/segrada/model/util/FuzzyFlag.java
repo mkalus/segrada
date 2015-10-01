75a99f120da816114432250811005c278a9721de
Maximilian Kalus <info@auxnet.de>
2015-10-01 21:40:33 +0200

    Add fuzzy flag enum and test for annotated fuzzy dates.

---
+++ src/main/java/org/segrada/model/util/FuzzyFlag.java
@@ -0,0 +1,110 @@
+package org.segrada.model.util;
+
+import java.util.EnumSet;
+
+/**
+ * Copyright 2015 Maximilian Kalus [segrada@auxnet.de]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Fuzzy flag implementation for fuzzy dates
+ */
+public enum FuzzyFlag {
+	FUZZY_CA('c'), // ca prefix
+	FUZZY_UNKNOWN('?'); // ? suffix
+
+	private final char fuzzyValue;
+
+	FuzzyFlag(char fuzzyValue) {
+		this.fuzzyValue = fuzzyValue;
+	}
+
+	public char getFuzzyValue() {
+		return fuzzyValue;
+	}
+
+	/**
+	 * translate char to fuzzy flag
+	 * @param flag input char
+	 * @return corresponding flag or null
+	 */
+	public static FuzzyFlag translateCharToFuzzyFlag(char flag) {
+		switch (flag) {
+			case 'c': return FUZZY_CA;
+			case '?': return FUZZY_UNKNOWN;
+		}
+		return null;
+	}
+
+	/**
+	 * helper to add to any container
+	 * @param flag to set
+	 * @param flagContainer to be set
+	 * @return changed container
+	 */
+	public static EnumSet<FuzzyFlag> addFuzzyFlag(char flag, EnumSet<FuzzyFlag> flagContainer) {
+		FuzzyFlag f = FuzzyFlag.translateCharToFuzzyFlag(flag);
+		if (f != null) {
+			// null container? return new one
+			if (flagContainer == null) return EnumSet.of(f);
+			// otherwise just add to set
+			flagContainer.add(f);
+		}
+		return flagContainer;
+	}
+
+	/**
+	 * helper to delete from any container
+	 * @param flag to delete
+	 * @param flagContainer to be set
+	 * @return changed container
+	 */
+	public static EnumSet<FuzzyFlag> deleteFuzzyFlag(char flag, EnumSet<FuzzyFlag> flagContainer) {
+		FuzzyFlag f = FuzzyFlag.translateCharToFuzzyFlag(flag);
+		if (f != null && flagContainer != null) {
+			// remove from container set
+			flagContainer.remove(f);
+			// all entries removed, remove container
+			if (flagContainer.isEmpty()) return null;
+		}
+		return flagContainer;
+	}
+
+	/**
+	 * helper to check if flag is contained in container
+	 * @param flag to check
+	 * @param flagContainer to be checked
+	 * @return true, if flag is in container
+	 */
+	public static boolean hasFuzzyFlag(char flag, EnumSet<FuzzyFlag> flagContainer) {
+		FuzzyFlag f = FuzzyFlag.translateCharToFuzzyFlag(flag);
+		return f != null && flagContainer != null && flagContainer.contains(f);
+	}
+
+	/**
+	 * helper to get all flags from container
+	 * @param flagContainer to be extracted
+	 * @return list of flags, can be empty array
+	 */
+	public static char[] getFuzzyFlags(EnumSet<FuzzyFlag> flagContainer) {
+		// empty? return empty
+		if (flagContainer == null || flagContainer.isEmpty()) return new char[0];
+		char[] values = new char[flagContainer.size()];
+		int i = 0;
+		for (FuzzyFlag fuzzyFlag : flagContainer) {
+			values[i++] = fuzzyFlag.getFuzzyValue();
+		}
+
+		return values;
+	}
+}
---
+++ src/test/java/org/segrada/model/util/FuzzyFlagTest.java
@@ -0,0 +1,103 @@
+package org.segrada.model.util;
+
+import org.junit.Test;
+
+import java.util.EnumSet;
+
+import static org.junit.Assert.*;
+
+public class FuzzyFlagTest {
+
+	@Test
+	public void testGetFuzzyValue() throws Exception {
+		assertEquals('c', FuzzyFlag.FUZZY_CA.getFuzzyValue());
+		assertEquals('?', FuzzyFlag.FUZZY_UNKNOWN.getFuzzyValue());
+	}
+
+	@Test
+	public void testTranslateCharToFuzzyFlag() throws Exception {
+		assertEquals(FuzzyFlag.FUZZY_CA, FuzzyFlag.translateCharToFuzzyFlag('c'));
+		assertEquals(FuzzyFlag.FUZZY_UNKNOWN, FuzzyFlag.translateCharToFuzzyFlag('?'));
+
+		assertNull(FuzzyFlag.translateCharToFuzzyFlag('a'));
+	}
+
+	@Test
+	public void testAddFuzzyFlag() throws Exception {
+		// add invalid value to null container
+		assertNull(FuzzyFlag.addFuzzyFlag('a', null));
+
+		// add valid value to null container
+		EnumSet<FuzzyFlag> testContainer = FuzzyFlag.addFuzzyFlag('c', null);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 1);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_CA));
+
+		// add another value
+		testContainer = FuzzyFlag.addFuzzyFlag('?', testContainer);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 2);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_CA));
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_UNKNOWN));
+
+		// add invalid value
+		testContainer = FuzzyFlag.addFuzzyFlag('a', testContainer);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 2);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_CA));
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_UNKNOWN));
+	}
+
+	@Test
+	public void testDeleteFuzzyFlag() throws Exception {
+		// remove invalid and valid value from null container
+		assertNull(FuzzyFlag.deleteFuzzyFlag('a', null));
+		assertNull(FuzzyFlag.deleteFuzzyFlag('c', null));
+
+		EnumSet<FuzzyFlag> testContainer = EnumSet.of(FuzzyFlag.FUZZY_CA, FuzzyFlag.FUZZY_UNKNOWN);
+
+		// remove invalid from container
+		testContainer = FuzzyFlag.deleteFuzzyFlag('a', testContainer);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 2);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_CA));
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_UNKNOWN));
+
+		// remove valid from container
+		testContainer = FuzzyFlag.deleteFuzzyFlag('c', testContainer);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 1);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_UNKNOWN));
+
+		// again
+		testContainer = FuzzyFlag.deleteFuzzyFlag('c', testContainer);
+		assertNotNull(testContainer);
+		assertTrue(testContainer.size() == 1);
+		assertTrue(testContainer.contains(FuzzyFlag.FUZZY_UNKNOWN));
+
+		// remove last element
+		testContainer = FuzzyFlag.deleteFuzzyFlag('?', testContainer);
+		assertNull(testContainer);
+	}
+
+	@Test
+	public void testHasFuzzyFlag() throws Exception {
+		// test null container
+		assertFalse(FuzzyFlag.hasFuzzyFlag('a', null));
+		assertFalse(FuzzyFlag.hasFuzzyFlag('c', null));
+
+		EnumSet<FuzzyFlag> testContainer = EnumSet.of(FuzzyFlag.FUZZY_CA);
+
+		assertFalse(FuzzyFlag.hasFuzzyFlag('a', testContainer));
+		assertTrue(FuzzyFlag.hasFuzzyFlag('c', testContainer));
+		assertFalse(FuzzyFlag.hasFuzzyFlag('?', testContainer));
+	}
+
+	@Test
+	public void testGetFuzzyFlags() throws Exception {
+		assertArrayEquals(new char[]{}, FuzzyFlag.getFuzzyFlags(null));
+		assertArrayEquals(new char[]{}, FuzzyFlag.getFuzzyFlags(EnumSet.noneOf(FuzzyFlag.class)));
+		assertArrayEquals(new char[]{'c'}, FuzzyFlag.getFuzzyFlags(EnumSet.of(FuzzyFlag.FUZZY_CA)));
+		assertArrayEquals(new char[]{'c', '?'}, FuzzyFlag.getFuzzyFlags(EnumSet.of(FuzzyFlag.FUZZY_CA, FuzzyFlag.FUZZY_UNKNOWN))); //TODO: does this work on every machine?
+	}
+}
