84a067155b5e904d6a0c4db682c16e17820ea19e
Maximilian Kalus <info@auxnet.de>
2015-10-01 14:51:33 +0200

    Fuzzy flags also for nodes and relations. Add comments to Periods and Locations.

--- src/main/resources/orientdb/schema_version_1.sql
+++ src/main/resources/orientdb/schema_version_1.sql
@@ -1,7 +1,18 @@
 ######################################################################################################################
-# Extend period with fuzzy entries
+# Extend period with fuzzy entries and period comment
 create property Period.fromFuzzyFlags String
 create property Period.toFuzzyFlags String
+create property Period.comment String
+
+create property Node.minFuzzyFlags String
+create property Node.maxFuzzyFlags String
+
+create property Relation.minFuzzyFlags String
+create property Relation.maxFuzzyFlags String
+
+######################################################################################################################
+# Location comment
+create property Location.comment String
 
 ######################################################################################################################
 # Forgotten file identifier added, plus thumbnail
