8174feca05f9217a6bf266afe7fef6589fabc69d
Maximilian Kalus <info@auxnet.de>
2015-10-06 14:49:48 +0200

    Create correct cache key for lists.

--- src/main/java/org/segrada/servlet/SegradaSimplePageCachingFilter.java
+++ src/main/java/org/segrada/servlet/SegradaSimplePageCachingFilter.java
@@ -3,11 +3,10 @@ package org.segrada.servlet;
 import net.sf.ehcache.constructs.blocking.LockTimeoutException;
 import net.sf.ehcache.constructs.web.AlreadyCommittedException;
 import net.sf.ehcache.constructs.web.AlreadyGzippedException;
-import net.sf.ehcache.constructs.web.Header;
 import net.sf.ehcache.constructs.web.PageInfo;
 import net.sf.ehcache.constructs.web.filter.FilterNonReentrantException;
 import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
-import org.eclipse.jetty.server.Request;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,8 +14,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.Serializable;
-import java.util.List;
+import java.net.URLEncoder;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,6 +48,11 @@ public class SegradaSimplePageCachingFilter extends SimplePageCachingFilter {
 	 */
 	private static final Pattern excludePatterns = Pattern.compile("/(clear_cache|reindex|locale/)");
 
+	/**
+	 * url parts that add a session key to the cache key in order to function properly
+	 */
+	private static final Pattern addSessionToCacheKey = Pattern.compile("^/(node|source|file|relation|relation_type|pictogram|tag|color)$");
+
 	@Override
 	protected void doFilter(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws AlreadyGzippedException, AlreadyCommittedException, FilterNonReentrantException, LockTimeoutException, Exception {
 		// exclude?
@@ -70,7 +78,11 @@ public class SegradaSimplePageCachingFilter extends SimplePageCachingFilter {
 		}
 	}
 
-
+	/**
+	 * calculate key for page from httpRequest
+	 * @param httpRequest the request
+	 * @return cache key
+	 */
 	protected String calculateKey(HttpServletRequest httpRequest) {
 		HttpSession session = httpRequest.getSession();
 
@@ -79,14 +91,55 @@ public class SegradaSimplePageCachingFilter extends SimplePageCachingFilter {
 		String language = lObject==null?null:(String) lObject;
 		if (language == null) language = httpRequest.getLocale().getLanguage();
 
-		StringBuffer stringBuffer = new StringBuffer();
-		stringBuffer.append(httpRequest.getMethod()).append(language).append(httpRequest.getRequestURI()).append(httpRequest.getQueryString());
-		String key = stringBuffer.toString();
+		// get url, context path stripped
+		String urlPart = httpRequest.getRequestURI().substring(httpRequest.getContextPath().length());
 
-		// replace ?page=1
-		if (key.endsWith("page=1"))
-			key = key.substring(0, key.length()-6).concat("null");
+		// query data map - later to be sorted into a key
+		SortedMap<String, String> queryData = new TreeMap<>();
 
-		return key;
+		// match with urls that require session key addition to cache in order to function properly?
+		MatchResult matchResult = addSessionToCacheKey.matcher(urlPart).toMatchResult();
+		if (((Matcher) matchResult).find()) {
+			String controller = matchResult.group().substring(1, 2).toUpperCase() + matchResult.group().substring(2) + "Service";
+			// get session data
+			Object controllerData = session.getAttribute(controller);
+
+			// create sorted map
+			if (controllerData != null && controllerData instanceof Map) {
+				Map<String, String> d = (Map<String, String>)controllerData;
+				for (String parameter : d.keySet()) {
+					queryData.put(parameter, d.get(parameter));
+				}
+			}
+		}
+
+		// get query data and add it to list (overwrite session, if needed)
+		Enumeration<String> parameters = httpRequest.getParameterNames();
+		while (parameters.hasMoreElements()) {
+			String parameter = parameters.nextElement();
+			String value = httpRequest.getParameter(parameter);
+
+			queryData.put(parameter, value);
+		}
+
+		// create query string as key
+		StringBuilder queryString = new StringBuilder();
+		for (Map.Entry<String, String> entry: queryData.entrySet()) {
+			try {
+				String encodedName = URLEncoder.encode(entry.getKey(), "UTF-8");
+				Object value = entry.getValue();
+				String encodedValue = value!=null?URLEncoder.encode(String.valueOf(value), "UTF-8"):"";
+				// do not include page=1
+				if (encodedName.equals("page") && encodedValue.equals("1")) continue;
+				if (queryString.length() > 0)
+					queryString.append('&');
+				queryString.append(encodedName).append("=").append(encodedValue);
+			} catch (Exception e) {
+				logger.warn("Could not encode field " + entry.getKey() + " with value " + entry.getValue(), e);
+			}
+		}
+
+		// create key
+		return httpRequest.getMethod() + language + urlPart + queryString.toString();
 	}
 }
