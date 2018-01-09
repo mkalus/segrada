a1a45c26d3bccbc93ceb22bef842fb3a28280926
Maximilian Kalus <info@auxnet.de>
2018-01-09 11:16:23 +0100

    Version bump to 0.2.3.

--- src/main/java/org/segrada/servlet/SegradaUpdateChecker.java
+++ src/main/java/org/segrada/servlet/SegradaUpdateChecker.java
@@ -40,7 +40,7 @@ public class SegradaUpdateChecker {
 	/**
 	 * current version
 	 */
-	public static final String currentVersion = "v0.2.2";
+	public static final String currentVersion = "v0.2.3";
 
 	private final OrientGraphFactory graph;
 
