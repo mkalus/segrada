d7afd12af5910b695891d8008b3ae3c9d8fcd7a9
Maximilian Kalus <info@auxnet.de>
2021-08-30 23:13:47 +0200

    Version bump to v0.5.1.

--- src/main/java/org/segrada/servlet/SegradaUpdateChecker.java
+++ src/main/java/org/segrada/servlet/SegradaUpdateChecker.java
@@ -40,7 +40,7 @@ public class SegradaUpdateChecker {
 	/**
 	 * current version
 	 */
-	public static final String currentVersion = "v0.5.0";
+	public static final String currentVersion = "v0.5.1";
 
 	private final OrientGraphFactory graph;
 
