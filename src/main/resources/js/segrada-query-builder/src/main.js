0dff2dde7b17ce67e2d293df78f66ad534c8f3de
Maximilian Kalus <info@auxnet.de>
2021-09-20 12:43:38 +0200

    Add store.

--- src/main/resources/js/segrada-query-builder/src/main.js
+++ src/main/resources/js/segrada-query-builder/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 
 // use function because we want to do this dynamically
 function createSegradaQueryBuilder (containerId) {
-  createApp(App).mount(containerId)
+  createApp(App).use(store).mount(containerId)
 }
 
 // export globally
